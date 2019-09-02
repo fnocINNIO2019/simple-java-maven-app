9e60c12588a207ec77e0c879a03ac95d5fd66d9e
fnocINNIO2019 <54308742+fnocINNIO2019@users.noreply.github.com>
2019-09-02 11:34:10 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,7 +13,6 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        System.out.println(password);
     }
 
     private final String getMessage() {
